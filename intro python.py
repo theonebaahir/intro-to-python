71c3a3d457b57da16e57554071076e8ee568a53c
theonebaahir <babusadudekula@gmail.com>
2025-03-28 21:22:58 +0530

    Add files via upload

---
+++ intro python.py
@@ -0,0 +1,24 @@
+# different print statements
+
+
+print("welcome to the python programming")
+print(2)
+print("\n hello world\n" )
+print("remy,",13)
+print("ground", end= "*")
+
+# variables
+r = "right"
+
+print(r)
+two = "baahir"
+print(two)
+num = 3
+print(num)
+
+name = input("enter your name")
+print("\n hello ", name , "\n welocome to codingal")
+# keywords
+import keyword
+print("python keywords are ... \n")
+print("keyword.Kwlist")
